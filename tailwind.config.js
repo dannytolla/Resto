44d096e4196598d22f3c2092960cc2df721a4785
Daniel Tolla <danieltda99@gmail.com>
2022-05-28 10:11:39 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["DM Sans", "sans-serif"],
+      },
+      colors: {
+        green: {
+          DEFAULT: "#71B214",
+        },
+        gray: {
+          600: "#606060",
+        },
+      },
+      spacing: {
+        13: "3.25rem",
+      },
+      boxShadow: {
+        primary: "0px 9.9px 21.6px rgba(136, 202, 41, 0.41)",
+      },
+    },
+  },
+  plugins: [],
+};
